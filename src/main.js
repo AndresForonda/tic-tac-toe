b9d4950fee94e9b12a89e3a3af2273a5535711a8
Andres Fernando <andresforonda.af@gmail.com>
2020-10-27 18:44:55 -0300

    Setup socket.io and toasted on Vue prototype

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueSocketIOExt from "vue-socket.io-extended";
+import io from "socket.io-client";
+
+const socket = io("http://192.168.1.116:3300");
+
+import Toasted from "vue-toasted";
+
+const toastedOptions = {
+  duration: 3000,
+};
+
+Vue.use(Toasted, toastedOptions);
+
+Vue.use(VueSocketIOExt, socket);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
