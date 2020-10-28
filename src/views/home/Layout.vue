84fa864c9285d522d657fc12752a64763737c322
Andres Fernando <andresforonda.af@gmail.com>
2020-10-28 19:26:03 -0300

    Create the home component and register it

---
+++ src/views/home/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <board />
+  </section>
+</template>
+<script>
+import Board from "./content/Layout";
+export default {
+  name: "Home",
+  components: {
+    Board
+  }
+};
+</script>
+<style scoped>
+section {
+  height: 80vh;
+  background: rgb(246, 119, 46);
+  background: linear-gradient(
+    0deg,
+    rgba(246, 119, 46, 1) 0%,
+    rgba(253, 45, 45, 1) 100%
+  );
+  border-radius: 0 0 0% 80%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
