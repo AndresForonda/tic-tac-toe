e2374d9ae7bec55b85bef1978583ddce2b4b946c
Andres Fernando <andresforonda.af@gmail.com>
2020-10-28 18:55:31 -0300

    Reset css and add google font

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <home />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./views/home/Layout";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Home
   }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Just+Another+Hand&display=swap");
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+body {
+  font-family: "PTSansRegular";
+  font-size: 14px;
+  color: #666666;
+}
+
+.left,
+.left-side,
+.pull-left {
+  float: left;
+}
+.right,
+.right-side,
+.pull-right {
+  float: right;
+}
+.clear {
+  clear: both;
+}
+.clearfix:after {
+  clear: both;
+  content: "";
+  display: block;
+}
+a img {
+  border: 0px;
+}
+a {
+  text-decoration: none;
+  color: #0d6bb2;
+}
+a:hover {
+  color: #b9baba;
+}
+
+.align-left {
+  text-align: left;
+}
+.align-center {
+  text-align: center;
+}
+.align-right {
+  text-align: right;
+}
+.vertical-align {
+  vertical-align: top;
+}
+.relative {
+  position: relative;
+}
+.absolute {
+  position: absolute;
+}
+
+li {
+  list-style-type: none;
+}
+.underline {
+  text-decoration: underline;
+}
+img {
+  max-width: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  /* margin-top: 60px; */
 }
 </style>
