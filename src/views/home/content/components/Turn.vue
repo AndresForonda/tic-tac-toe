8bc24055eb60397c9eb3d1525761f04f4673cc79
Andres Fernando <andresforonda.af@gmail.com>
2020-10-30 08:36:36 -0300

    Add turn component to show who is playing

---
+++ src/views/home/content/components/Turn.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Turn {{ player }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Turn",
+  props: {
+    number: {
+      type: String,
+      default: ""
+    },
+    player: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+h2 {
+  padding-top: 20px;
+  font-size: 120px;
+  font-family: "Just Another Hand", cursive;
+  color: #ffffff;
+}
+</style>
